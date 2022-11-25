2f851408a63c3b714d86b5d593a70c2e2c82f7d7
Nithya G <67510305+Nithya-G-coder@users.noreply.github.com>
2022-11-25 14:32:04 +0530

    Add files via upload

---
+++ Project-Development Phase/Sprint 3/connection.py
@@ -0,0 +1,3 @@
+import ibm_db
+
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=b1bc1829-6f45-4cd4-bef4-10cf081900bf.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32304;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=frb18707;PWD=GhfEkngvfCdiA9Y9",'','')
